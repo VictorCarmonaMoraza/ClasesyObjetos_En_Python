43db15881033036be215aba4086d87bd4d33a93c
Victor <VictorCarmona9035@hotmail.com>
2023-07-23 21:26:02 +0200

    3-Creacion de Objetos con argumentos - parte 3

--- Persona.py
+++ Persona.py
@@ -2,15 +2,15 @@
 # Creacion de una clase
 class Persona:
     #Metodo inicializador
-    def __init__(self):
+    def __init__(self, nombre, apellido, edad):
         #Atributos
-        self.nombre = 'Juan'
-        self.apellido = 'Perez'
-        self.edad = 28
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
 
 
 #Creacion de un objeto de esta clase
-persona1 = Persona()
+persona1 = Persona('Juan','apellido',28)
 
 #Imprimimos valores de la instancia persona1
 print(persona1.nombre)
