777c865564ebdf3a4e951f25b73ccf35948269cb
Victor <VictorCarmona9035@hotmail.com>
2023-07-23 21:07:55 +0200

    2-Clases y Objetos en Python - parte 2

--- Persona.py
+++ Persona.py
@@ -1,6 +1,18 @@
 
 # Creacion de una clase
 class Persona:
-    pass
+    #Metodo inicializador
+    def __init__(self):
+        #Atributos
+        self.nombre = 'Juan'
+        self.apellido = 'Perez'
+        self.edad = 28
 
-print(type(Persona))
+
+#Creacion de un objeto de esta clase
+persona1 = Persona()
+
+#Imprimimos valores de la instancia persona1
+print(persona1.nombre)
+print(persona1.apellido)
+print(persona1.edad)
