4a57dc203f311e6689d03399d519d042adb019c6
Victor <VictorCarmona9035@hotmail.com>
2023-07-24 20:15:43 +0200

    4-Creacion de mas objetos de una clase

--- Persona.py
+++ Persona.py
@@ -10,9 +10,13 @@ class Persona:
 
 
 #Creacion de un objeto de esta clase
-persona1 = Persona('Juan','apellido',28)
+persona1 = Persona('Juan','Carmona',28)
 
 #Imprimimos valores de la instancia persona1
-print(persona1.nombre)
-print(persona1.apellido)
-print(persona1.edad)
+print(f'Objeto numero 1 {persona1.nombre} {persona1.apellido} {persona1.edad}')
+
+#Creacion de un segundo objeto de tipo Persona
+persona2 = Persona('Karla','Gomez',45)
+
+#Imprimimos valores de la instancia persona2
+print(f'Objeto numero 2 {persona2.nombre} {persona2.apellido} {persona2.edad}')
