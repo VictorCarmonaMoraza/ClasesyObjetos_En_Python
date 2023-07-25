a09c8ab06b0d9799dc363e9130d71714238c510e
Victor <VictorCarmona9035@hotmail.com>
2023-07-25 10:16:55 +0200

    6-Metodos de instancia en Python

--- Persona.py
+++ Persona.py
@@ -8,23 +8,19 @@ class Persona:
         self.apellido = apellido
         self.edad = edad
 
+    def mostrar_detalle(self):
+        print(f'Persona: {self.nombre} {self.apellido} {self.edad}')
+
 
 #Creacion de un objeto de esta clase
 persona1 = Persona('Juan','Carmona',28)
 
 #Imprimimos valores de la instancia persona1
-print(f'Objeto numero 1 {persona1.nombre} {persona1.apellido} {persona1.edad}')
+persona1.mostrar_detalle()
 
-
-
-persona1.nombre = 'Victor Manuel'
-persona1.apellido = 'Carmona'
-persona1.edad = 42
-#Imprimimos el objeto con los nuevos valores
-print(f'Objeto numero 1 {persona1.nombre} {persona1.apellido} {persona1.edad}')
 
 #Creacion de un segundo objeto de tipo Persona
 persona2 = Persona('Karla','Gomez',45)
 
 #Imprimimos valores de la instancia persona2
-print(f'Objeto numero 2 {persona2.nombre} {persona2.apellido} {persona2.edad}')
+persona2.mostrar_detalle()
