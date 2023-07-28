a94eef3fb01d601ebd1048a541c9b3284050a6be
Victor <VictorCarmona9035@hotmail.com>
2023-07-28 20:34:16 +0200

    9-Ejercicio Clase Aritmetica

--- Aritmetica.py
+++ Aritmetica.py
@@ -7,10 +7,24 @@ class Aritmetica:
         self.operandoA = operandoA
         self.operandoB = operandoB
 
+    #Operacion Suma
     def sumar(self):
         return self.operandoA + self.operandoB
 
+    #Operacion Resta
+    def restar(self):
+        return  self.operandoA - self.operandoB
 
+    #Operacion Multiplicacion
+    def multiplicar(self):
+        return self.operandoA * self.operandoB
+
+    #Operacion Division
+    def dividir(self):
+        return  self.operandoA / self.operandoB
 
 aritmetica1 = Aritmetica(5, 3)
-print(aritmetica1.sumar())
+print(f'Suma: {aritmetica1.sumar()}')
+print(f'Resta: {aritmetica1.restar()}')
+print(f'Multiplicacion: {aritmetica1.multiplicar()}')
+print(f'Division: {aritmetica1.dividir():.2f}')
