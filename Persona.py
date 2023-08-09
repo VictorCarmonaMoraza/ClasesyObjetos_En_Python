517754f0c7a0f5545a7dbd32b18f771188389f33
Victor <VictorCarmona9035@hotmail.com>
2023-08-09 21:19:09 +0200

    Metodo Getter en atributo nombre

--- Persona.py
+++ Persona.py
@@ -4,9 +4,13 @@ class Persona:
     #Metodo inicializador
     def __init__(self, nombre, apellido, edad):
         #Atributos
-        self.__nombre = nombre
+        self._nombre = nombre
         self.apellido = apellido
         self.edad = edad
+
+    @property
+    def nombre(self):
+        return self._nombre
 
     def mostrar_detalle(self):
         print(f'Persona: {self.__nombre} {self.apellido} {self.edad}')
@@ -14,7 +18,4 @@ class Persona:
 
 #Creacion de un objeto de esta clase
 persona1 = Persona('Juan','Carmona',28)
-
-#Con doble guion bajo no podemos modificar el valor del parametro
-persona1.__nombre ="Felix"
-persona1.mostrar_detalle()
+print(persona1.nombre)
