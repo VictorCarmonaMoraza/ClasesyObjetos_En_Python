5eb3275b1f17142fda5eef0f5b1c50980d85df1f
Victor <VictorCarmona9035@hotmail.com>
2023-08-09 21:28:43 +0200

    Limpieza codigo

--- Persona.py
+++ Persona.py
@@ -1,9 +1,6 @@
 
-# Creacion de una clase
 class Persona:
-    #Metodo inicializador
     def __init__(self, nombre, apellido, edad):
-        #Atributos
         self._nombre = nombre
         self.apellido = apellido
         self.edad = edad
@@ -18,12 +15,8 @@ class Persona:
         print('Lllamndo setter nombre')
         self._nombre = nombre
 
-    def mostrar_detalle(self):
-        print(f'Persona: {self.__nombre} {self.apellido} {self.edad}')
 
 
-#Creacion de un objeto de esta clase
 persona1 = Persona('Juan','Carmona',28)
-#print(persona1.nombre)
 persona1.nombre ='Lucia'
 print(persona1.nombre)
