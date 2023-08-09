1dcabc4e1b89e65a76e4f573d398324f1030a37d
Victor <VictorCarmona9035@hotmail.com>
2023-08-09 20:55:00 +0200

    Encapsulamiento

--- Persona.py
+++ Persona.py
@@ -2,30 +2,19 @@
 # Creacion de una clase
 class Persona:
     #Metodo inicializador
-    def __init__(self, nombre, apellido, edad, *valoresTupla, **valoresDiccionario):
+    def __init__(self, nombre, apellido, edad):
         #Atributos
-        self.nombre = nombre
+        self.__nombre = nombre
         self.apellido = apellido
         self.edad = edad
-        self.tupla = valoresTupla
-        self.diccionario = valoresDiccionario
 
     def mostrar_detalle(self):
-        print(f'Persona: {self.nombre} {self.apellido} {self.edad} {self.tupla} {self.diccionario}')
+        print(f'Persona: {self.__nombre} {self.apellido} {self.edad}')
 
 
 #Creacion de un objeto de esta clase
-persona1 = Persona('Juan','Carmona',28, '4455225515',2,5,9,10, p='pedro',s='juan')
+persona1 = Persona('Juan','Carmona',28)
 
-#Imprimimos valores de la instancia persona1
+#Con doble guion bajo no podemos modificar el valor del parametro
+persona1.__nombre ="Felix"
 persona1.mostrar_detalle()
-#Persona.mostrar_detalle(persona1)
-persona1.telefono = '666 666 777'
-print(persona1.telefono)
-
-
-#Creacion de un segundo objeto de tipo Persona
-persona2 = Persona('Karla','Gomez',45)
-
-#Imprimimos valores de la instancia persona2
-persona2.mostrar_detalle()
